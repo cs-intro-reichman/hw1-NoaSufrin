f5bc12e89fe42b59a6860b4f1e6f2873519ea1a2
Noa Sufrin <noa.sufrin@post.runi.ac.il>
2024-11-08 14:48:07 +0200

    Completed project

--- NumWords.java
+++ NumWords.java
@@ -3,7 +3,7 @@ public class NumWords {
 	public static void main(String args[]) {
 		int number = Integer.parseInt(args[0]);
 
-		int hundereds = number / 100;
+		int hundreds = number / 100;
 		int tens = (number % 100) / 10; 
 		int ones = (number % 10);
 
